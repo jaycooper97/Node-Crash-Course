d64a114ac022f5283a5a40d25befbd356447d8ba
jay cooper <15jacooper@gmail.com>
2023-10-25 12:54:26 -0400

    push

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // const express = require('express');
+// const morgan = require('morgan');
+
+//register view engine
+//app.set('view engine', 'ejs');
+
 
 
 // const app = express();
@@ -7,11 +12,41 @@
 // //listen for request
 // app.listen(3000);
 
-// //home page
+//middleware and static files
+application.use(express.static('public'));
+
+
+
+application.use(morgan('dev'));
+
+
+
+//middleware
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path:', req.path);
+//     console.log('method:', req.method)
+//     next();
+// });
+
+
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//     next();
+// });
+
+
+//home page
 // app.get('/', (req, res) => {
+    //const blogs = [
+        // {title: 'Yoshi found eggs', snippet: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto nihil dolore quod, blanditiis dolorum inventore tempora qui ex laborum sint nulla, sunt molestias officia quos doloremque nesciunt porro dolor! Ratione.'}
+        // {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto nihil dolore quod, blanditiis dolorum inventore tempora qui ex laborum sint nulla, sunt molestias officia quos doloremque nesciunt porro dolor! Ratione.'}
+        // {title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto nihil dolore quod, blanditiis dolorum inventore tempora qui ex laborum sint nulla, sunt molestias officia quos doloremque nesciunt porro dolor! Ratione.'}
+   // ];
 //     //res.send('<p>Home Page</p>');
 
-//     res.sendFile('./views/index.html', {root: __dirname});
+//     res.render('idex', { title: 'Home', blogs});
 // });
 
 
@@ -19,17 +54,16 @@
 // app.get('/about', (req, res) => {
 //     //res.send('<p>About Page</p>');  //HTML
 
-//     res.sendFile('./views/about.html');   //actual file
+//     res.render('about', { title: 'About' })   //actual file
 // });
 
 
-// //redirect
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// });
+// application.get('/blogs', (req, res) => {
+//     res.render('create', { title: 'Create a New Blog' });
+// })
 
 // //404
 // app.use((req, res) => {
-//     res.status(404).sendFile('./views/404.html', {root: __dirname});   //sends 404 page to browser
+//     res.status(404).render('404', { title: '404' });   //sends & renders 404 page to browser
 
 // });
